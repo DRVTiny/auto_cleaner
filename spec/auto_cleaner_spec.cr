9e057c29fdfa72ab30f3297bacba1e13d3a8a074
Andrey A. Konovalov <drvtiny@gmail.com>
2019-05-08 21:16:23 +0300

    More clever spec appropriate to Crystal tests standarts

--- spec/auto_cleaner_spec.cr
+++ spec/auto_cleaner_spec.cr
@@ -1,21 +1,41 @@
 require "./spec_helper"
 require "../src/auto_cleaner"
 describe Auto::Cleaner do
-  in_p, out_p = IO.pipe
-  child_p = Process.fork do
-  	ac = Auto::Cleaner.new
-  	tmp_f = File.tempfile("ac-test", ".tst")
-  	ac.add_file tmp_f.path
-  	tmp_f.puts "Auto::Cleaner glad to test itself again!"
-  	tmp_f.close
-  	out_p.puts tmp_f.path
-  	exit 0  	
+	describe "auto cleaning on exit" do
+    in_p, out_p = IO.pipe
+    child_p = Process.fork do
+      ac = Auto::Cleaner.new
+      tmp_f = File.tempfile("ac-test", ".tst")
+      ac.add_file tmp_f.path
+      tmp_f.puts "Auto::Cleaner glad to test itself again!"
+      tmp_f.close
+      out_p.puts tmp_f.path
+      exit 0  	
+    end
+    child_p.wait
+    tmp_f_path = in_p.gets
+    tmp_f_path.class.should eq(String)
+    it "should automa(g|t)ically remove temporary file after exit" do
+      File.exists?(tmp_f_path.not_nil!).should be_false
+    end
   end
-  child_p.wait
-  tmp_f_path = in_p.gets
-  tmp_f_path.class.should eq(String)
-  if tmp_f_path
-  	STDERR.puts "temp file path: #{tmp_f_path}"
-	  File.exists?(tmp_f_path).should eq(false)
-	end
+  
+  describe "#make_mrproper" do
+  	ac = Auto::Cleaner.new
+  	count = 0
+  	ac.add_proc(2) {|to_add| count += to_add }
+  	ac.make_mrproper
+  	it "should run user-defined proc as a result #make_mrproper method call" do
+	  	count.should eq(2)
+	  end
+	  ac.make_mrproper
+	  it "should NOT run user-defined cleaning proc twice (and should not do anything) if make_mrproper call followed by another call to the same method without any add_* methods invocations between this calls" do
+	  	count.should eq(2)
+	  end
+	  ac.add_file(tmp_f_path = File.tempfile.path)
+	  ac.make_mrproper
+	  it "should remove temporary files added using add_file method after call to make_mrproper" do
+	  	File.exists?(tmp_f_path.not_nil!).should be_false
+	  end
+  end
 end
